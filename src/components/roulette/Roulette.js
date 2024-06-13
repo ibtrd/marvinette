ff55c30550db2f7025ded5c8495b0bbc3c8d4f83
Bastien Wisniewski <wisniewski.bastien31@gmail.com>
2024-06-13 22:50:01 +0200

    Edit roulette animation

--- src/components/roulette/Roulette.js
+++ src/components/roulette/Roulette.js
@@ -5,45 +5,79 @@ import { animated, useSpring, easings } from 'react-spring'
 export default function Roulette() {
 
 	const	cells = [
-		{name: 'Goal0', weigth: '40'},
-		{name: 'Goal1', weigth: '10'},
-		{name: 'Goal2', weigth: '-20'},
-		{name: 'Goal3', weigth: '-30'},
-		{name: 'Goal4', weigth: '40'},
-		{name: 'Goal5', weigth: '40'},
-		{name: 'Goal6', weigth: '10'},
-		{name: 'Goal7', weigth: '-20'},
-		{name: 'Goal8', weigth: '-30'},
-		{name: 'Goal9', weigth: '-20'},
-		{name: 'Goal10', weigth: '-30'},
-		{name: 'Goal11', weigth: '40'},
-		{name: 'Goal12', weigth: '10'},
-		{name: 'Goal13', weigth: '-20'},
-		{name: 'Goal14', weigth: '-30'},
-		{name: 'Goal15', weigth: '-20'}
+		{name: 'Goal 0', weigth: '40'},
+		{name: 'Goal 1', weigth: '10'},
+		{name: 'Goal 2', weigth: '-20'},
+		{name: 'Goal 3', weigth: '-30'},
+		{name: 'Goal 4', weigth: '40'},
+		{name: 'Goal 5', weigth: '40'},
+		{name: 'Goal 6', weigth: '10'},
+		{name: 'Goal 7', weigth: '-20'},
+		{name: 'Goal 8', weigth: '-30'},
+		{name: 'Goal 9', weigth: '-20'},
+		{name: 'Goal 10', weigth: '-30'},
+		{name: 'Goal 11', weigth: '40'},
+		{name: 'Goal 12', weigth: '10'},
+		{name: 'Goal 13', weigth: '-20'},
+		{name: 'Goal 14', weigth: '-30'},
+		{name: 'Goal 15', weigth: '-20'},
+		{name: 'Goal 16', weigth: '40'},
+		{name: 'Goal 17', weigth: '10'},
+		{name: 'Goal 18', weigth: '-20'},
+		{name: 'Goal 19', weigth: '-30'},
+		{name: 'Goal 20', weigth: '40'},
+		{name: 'Goal 21', weigth: '40'},
+		{name: 'Goal 22', weigth: '10'},
+		{name: 'Goal 23', weigth: '-20'},
+		{name: 'Goal 24', weigth: '-30'},
+		{name: 'Goal 25', weigth: '-20'},
+		{name: 'Goal 26', weigth: '-30'},
+		{name: 'Goal 27', weigth: '40'},
+		{name: 'Goal 28', weigth: '10'},
+		{name: 'Goal 29', weigth: '-20'},
+		{name: 'Goal 30', weigth: '-30'},
+		{name: 'Goal 31', weigth: '-20'},
+		{name: 'Goal 32', weigth: '-20'},
+		{name: 'Goal 33', weigth: '-20'},
+		{name: 'Goal 34', weigth: '-20'},
+		{name: 'Goal 35', weigth: '-20'},
+		{name: 'Goal 36', weigth: '-20'},
+		{name: 'Goal 37', weigth: '-20'},
+		{name: 'Goal 38', weigth: '-20'},
+		{name: 'Goal 39', weigth: '-20'},
+		{name: 'Goal 40', weigth: '-20'},
+		{name: 'Goal 41', weigth: '-20'},
 	]
 
 	const getRandomDuration = (min, max) => {
 		return (Math.random() * (max - min) + min) * 1000;
 	}
 
+	const getRandom = (min, max) => {
+		return (Math.random() * (max - min) + min);
+	}
+
+	const randomStart = getRandom(0, 360);
+
 
 	const [springs, api] = useSpring(() => ({
-		from: { rotate: 0 },
+		from: { rotate: randomStart },
 	}))
 
 	const	handleClick = () => {
-		var	goal = Math.floor(Math.random() * (cells.length));
-		console.log('Goal:', cells[goal].name, goal);
+		const	goal = Math.floor(Math.random() * (cells.length));
+		const rand =  getRandom((-360/cells.length)/2.5, (360/cells.length)/2.5)
+		console.log('Goal:', cells[goal].name, goal, 'rand:', rand);
 		api.start({
 			from: {
-				rotate: 0,
+				rotate: randomStart,
 			},
 			to: {
-				rotate: 360 * 20 + ((360/cells.length) * goal),
+				// rotate: 360 * 20 + ((360/cells.length) * goal),
+				rotate: 360 * 10 + (360 - (360/cells.length) * goal) + rand,
 			},
 			config: {
-				duration: getRandomDuration(7, 14),
+				duration: getRandomDuration(10, 15),
 				easing: easings.easeOutCubic,
 			},
 		})
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Roulette from "../components/roulette/Roulette";
 import '../App.css'
+import RoulettePointer from "../components/roulette/RoulettePointer";
 
 export default function Home() {
 
   return (
     <div className='Home'>
       <Roulette />
+      <RoulettePointer />
     </div>
     
   );
