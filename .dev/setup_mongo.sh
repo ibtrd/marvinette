bcae4a5a793f4aceba2c8310ce7167407e9dddbb
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-06-18 13:25:00 +0200

    Add bash script to create/start mongo docker

---
+++ .dev/setup_mongo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+STATUS=$(docker ps -a | grep mongo)
+if [ -z "$STATUS" ]; then
+    docker run --name mongo -d -it -p 27017:27017 mongo
+else
+    STATUS=$(echo "$STATUS" | grep Exited)
+	if [ -z "$STATUS" ]; then
+		echo mongo docker is already running
+	else
+		docker start mongo
+	fi
+fi
