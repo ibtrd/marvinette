fc3f79ddf512da7ffde557d78f1a3a852cfbd70a
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-06-18 13:28:13 +0200

    Fix database name

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const router = require('./routes/router');
 const { rouletteInterval } = require('./roulette/rouletteCells') 
 
 async function connectDB() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/ft_wordle");
+  await mongoose.connect("mongodb://127.0.0.1:27017/42roulette");
 }
 
 connectDB().catch((err) => {
