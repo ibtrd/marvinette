e545f4723bbfa1301113d12ce47162a99e54159d
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-06-26 02:09:24 +0200

    Fix isLoggedIn middleware game status checl

--- server/middleware/isLoggedIn.js
+++ server/middleware/isLoggedIn.js
@@ -1,8 +1,8 @@
 const Settings = require("../mongo_models/Settings");
 
-module.exports = function isLoggedIn(req, res, next) {
-	const status = Settings.findByKey('gameStatus');
-	if (status != 'active') {
+module.exports = async function isLoggedIn(req, res, next) {
+	const status = await Settings.findByKey('gameStatus');
+	if (!status || status.value != 'active') {
 		return res.redirect('/login'); //Add inactive game page redirection
 	} if (req.session.user) {
 		next();
