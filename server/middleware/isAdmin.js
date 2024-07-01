55b7672ec0eefac7e69cb90c4b25a0992376dc8b
Ian <128724498+ibtrd@users.noreply.github.com>
2024-07-02 01:59:23 +0200

    Add chamption to /stats route

--- server/middleware/isAdmin.js
+++ server/middleware/isAdmin.js
@@ -2,12 +2,12 @@ const Profile = require("../mongo_models/Profile");
 
 module.exports = async function isAdmin(req, res, next) {
 	if (!req.session.user) {
-		return (res.status(401 ).send('Unauthorized'));
+		return res.status(401).send();
 	}
 	const profile = await Profile.findByLogin(req.session.user.login);
 	if (profile && profile['admin?'] === true) {
 		next()
 	} else {
-		return res.status(403).send('Forbidden');
+		return res.status(403).send();
 	}
 }
--- server/routes/sendStats.js
+++ server/routes/sendStats.js
@@ -1,4 +1,5 @@
 const Profile = require("../mongo_models/Profile");
+const Rewards = require("../mongo_models/Rewards");
 const Settings = require("../mongo_models/Settings");
 
 module.exports = async function sendStats(req, res) {
@@ -8,17 +9,19 @@ module.exports = async function sendStats(req, res) {
         res.status(500).send();
     } else {
         const total = await Profile.getTotalSpins(poolYear, poolMonth);
-        const Worms = await Profile.getCoalitionSpins('The Worms', poolYear, poolMonth);
-        const Blobfishes = await Profile.getCoalitionSpins('The Blobfishes', poolYear, poolMonth);
-        const Skunks = await Profile.getCoalitionSpins('The Skunks', poolYear, poolMonth);
-        // const champion = Profile.find({ "champion?" === true})
+        const worms = await Profile.getCoalitionSpins('The Worms', poolYear, poolMonth);
+        const blobfishes = await Profile.getCoalitionSpins('The Blobfishes', poolYear, poolMonth);
+        const skunks = await Profile.getCoalitionSpins('The Skunks', poolYear, poolMonth);
+        const champion = await Rewards.getCurrentChampion(poolYear, poolMonth);
         res.send({ 
             total: total, 
-            theWorms: Worms, 
-            theBlobfishes: Blobfishes, 
-            theSkunks: Skunks
-            // champion: champion;
+            theWorms: worms, 
+            theBlobfishes: blobfishes, 
+            theSkunks: skunks,
+            champion: {
+                login: champion ? champion.login : null,
+                img: champion ? champion.img : null
+            },
         });
     }
 }
-
