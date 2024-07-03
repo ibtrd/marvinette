93e73450425f4959cbd6f46c2e946b5f38437c09
Ian <128724498+ibtrd@users.noreply.github.com>
2024-07-03 23:31:44 +0200

    Add coalition backgrounds

--- server/auth/config.js
+++ server/auth/config.js
@@ -41,19 +41,22 @@ const piscineCoalitions = [
     id: 15,
     name: "The Blobfishes",
     img: "/cellsIcons/blobfishes.png",
-    color: "#82cce0"
+    color: "#82cce0",
+    background: "https://cdn.intra.42.fr/coalition/cover/15/sloths_background.jpg"
   },
   {
     id: 16,
     name: "The Skunks",
     img: "/cellsIcons/skunks.png",
-    color: "#6c8946"
+    color: "#6c8946",
+    background: "https://cdn.intra.42.fr/coalition/cover/16/skunk_background.jpg"
   },
   {
     id: 17,
     name: "The Worms",
     img: "/cellsIcons/worms.png",
-    color: "#eab77f"
+    color: "#eab77f",
+    background: "https://cdn.intra.42.fr/coalition/cover/17/worms_background.jpg",
   },
 ];
 
