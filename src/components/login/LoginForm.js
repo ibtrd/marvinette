74c04e9b9d3aaa53f4d27a0db5b568cd38ebe443
Ian <128724498+ibtrd@users.noreply.github.com>
2024-07-06 14:48:35 +0200

    Add linebreak to welcome message
    Co-authored-by: Bastien Wisniewski <UnRenardQuiDab@users.noreply.github.com>

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -12,12 +12,13 @@ export default function LoginForm({...props}) {
     >
       <Heading>Marvinette</Heading>
       <Text marginY="64px" textAlign="justify" w={["60vw", "40vw", "30vw"]}>
-        Dear 42 Lyon's Pisciner. By now, you must be well-acquainted with
-        Norminette and her daunting sister, Moulinette. We all know that luck
-        has absolutely no part in dealing with them. But here's some exciting
-        news for you! This year, their long-lost cousin is joining the lineup.
-        What does she do, you ask? We'll leave that for you to discover, but
-        maybe this time luck might just have a role to play :)
+        Dear 42 Lyon's Pisciner. <br />
+        By now, you must be well-acquainted with Norminette and her daunting
+        sister, Moulinette. We all know that luck has absolutely no part in
+        dealing with them. But here's some exciting news for you! This year,
+        their long-lost cousin is joining the lineup. What does she do, you ask?
+        We'll leave that for you to discover, but maybe this time luck might
+        just have a role to play :)
       </Text>
       <LoginButton />
     </Flex>
